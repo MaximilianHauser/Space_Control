80e92f64aed4af4fded99e5e185a2e6e6beb8761
MaximilianHauser <113019206+MaximilianHauser@users.noreply.github.com>
2022-09-09 07:23:04 +0200

    Create hexlogic.py

---
+++ hexlogic.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 07:19:00 2022
+
+Will contain all hextile logic, such as distances, neighbors, range
+
+@author: Maximilian
+"""
+
+class HexLogic:
+    pass
