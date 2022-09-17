391236806eb70b619eaece7a7a9fe2e93b968ec1
MaximilianHauser <113019206+MaximilianHauser@users.noreply.github.com>
2022-09-17 20:46:08 +0200

    added r templates to u_attr.py

--- u_attr.py
+++ u_attr.py
@@ -132,6 +132,120 @@ u_dict = {
             "dmg_per_shot" : 0,
             "starting_ap" : 1,
             "action_points" : 1,
+            },
+        
+    # BLUFOR ships ---------------------------------------------------------- #
+ 
+    "r_cc":
+        {
+            "image" : "game.sprite_blufor_CC",
+            "ship_size_light" : False,
+            "ship_size_medium" : False,
+            "ship_size_heavy" : True,
+            
+            "max_health" : 10,
+            "health" : 10,
+            "dmg_per_shot" : 6,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_bc":
+        {
+            "image" : "game.sprite_blufor_BC",
+            "ship_size_light" : False,
+            "ship_size_medium" : False,
+            "ship_size_heavy" : True,
+            
+            "max_health" : 8,
+            "health" : 8,
+            "dmg_per_shot" : 6,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_ac":
+        {
+            "image" : "game.sprite_blufor_AC",
+            "ship_size_light" : False,
+            "ship_size_medium" : False,
+            "ship_size_heavy" : True,
+            
+            "max_health" : 6,
+            "health" : 6,
+            "dmg_per_shot" : 1,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_dd":
+        {
+            "image" : "game.sprite_blufor_DD",
+            "ship_size_light" : False,
+            "ship_size_medium" : True,
+            "ship_size_heavy" : False,
+            
+            "max_health" : 5,
+            "health" : 5,
+            "dmg_per_shot" : 3,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_ff":
+        {
+            "image" : "game.sprite_blufor_FF",
+            "ship_size_light" : False,
+            "ship_size_medium" : True,
+            "ship_size_heavy" : False,
+            
+            "max_health" : 4,
+            "health" : 4,
+            "dmg_per_shot" : 2,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_cv":
+        {
+            "image" : "game.sprite_blufor_CC",
+            "ship_size_light" : True,
+            "ship_size_medium" : False,
+            "ship_size_heavy" : False,
+            
+            "max_health" : 3,
+            "health" : 3,
+            "dmg_per_shot" : 2,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_gs":
+        {
+            "image" : "game.sprite_blufor_GS",
+            "ship_size_light" : True,  # adjust ship size for armor?
+            "ship_size_medium" : False,
+            "ship_size_heavy" : False,
+            
+            "max_health" : 2,
+            "health" : 2,
+            "dmg_per_shot" : 3,
+            "starting_ap" : 1,
+            "action_points" : 1,
+            },
+        
+    "r_tt":
+        {
+            "image" : "game.sprite_blufor_TT",
+            "ship_size_light" : False,
+            "ship_size_medium" : True,
+            "ship_size_heavy" : False,
+            
+            "max_health" : 3,
+            "health" : 3,
+            "dmg_per_shot" : 0,
+            "starting_ap" : 1,
+            "action_points" : 1,
             }
     
     }
